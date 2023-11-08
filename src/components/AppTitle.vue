1855625f933889480f6d4d6f6567af37d0bd30db
IIODERII <oderrisi@gmail.com>
2023-11-08 14:28:04 +0100

    fixed image with text

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -17,6 +17,11 @@ export default {
         color: red;
         text-align: center;
         font-size: 6em;
-        
+        position: absolute;
+        top: 12%;
+        left: 50%;
+        width: 45%;
+        transform: translateX(-50%);
+        font-weight: 700;
     }
 </style>
