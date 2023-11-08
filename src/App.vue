4f759cb21ab9125cf720967bc453639c053d9dc0
IIODERII <oderrisi@gmail.com>
2023-11-08 14:08:19 +0100

    added the first component

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AppTitle from './components/AppTitle.vue'
+</script>
+
+<template>
+  <AppTitle/>
+</template>
+
+<style scoped>
+#app{
+  text-align: center;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {title: "La mia prima app con Vite!"}
+    },
+}
+</script>
+
+<style lang="css" scoped>
+    h1{
+        color: red;
+        text-align: center;
+    }
+</style>
